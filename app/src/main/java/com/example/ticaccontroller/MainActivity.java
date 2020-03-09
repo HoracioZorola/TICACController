2c5340913d7dd600d5acafd9357589a41bf62c9f
Usuario <301710017@bis.ute.edu.mx>
2020-03-09 17:04:34 -0600

    Creación pantalla principal

--- app/src/main/java/com/example/ticaccontroller/MainActivity.java
+++ app/src/main/java/com/example/ticaccontroller/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.ticaccontroller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Object Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.main_screen);
+            }
+        });
     }
 }
